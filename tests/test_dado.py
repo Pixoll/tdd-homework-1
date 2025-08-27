110f4cc5bdbaf2be8be12f76ad07c25650a049d6
Pixoll <pixoll.gamer@gmail.com>
2025-08-27 17:46:27 -0400

    RED: test para guardar valor del dado al tirar - falla como esperado

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -3,5 +3,7 @@ from src.juego.dado import Dado
 
 def test_tirar_dado() -> None:
     dado = Dado()
-    value = dado.tirar()
-    assert 1 <= value <= 6
+    valor = dado.tirar()
+    assert 1 <= valor <= 6
+    assert 1 <= dado.valor <= 6
+    assert valor == dado.valor
