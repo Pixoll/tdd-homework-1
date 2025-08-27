525d4808bd1f726b6832fd0fc5e90421979f373e
Pixoll <pixoll.gamer@gmail.com>
2025-08-27 17:47:22 -0400

    GREEN: implementación mínima para guardar valor al tirar el dado

--- src/juego/dado.py
+++ src/juego/dado.py
@@ -2,6 +2,14 @@ from random import randint
 
 
 class Dado:
-    @staticmethod
-    def tirar() -> int:
-        return randint(1, 6)
+    def __init__(self) -> None:
+        self._valor: int | None = None
+
+    def tirar(self) -> int:
+        value = randint(1, 6)
+        self._valor = value
+        return value
+
+    @property
+    def valor(self) -> int | None:
+        return self._valor
