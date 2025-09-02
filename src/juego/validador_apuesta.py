37a69f08f2de8c6d79ffd4cec9dc4fe71b7f28d5
Pixoll <pixoll.gamer@gmail.com>
2025-09-02 12:10:19 -0400

    GREEN: implementación mínima para manejar casos especiales en validacion de apuestas

--- src/juego/validador_apuesta.py
+++ src/juego/validador_apuesta.py
@@ -15,41 +15,30 @@ class ValidadorApuesta:
         return cantidad >= 1
 
     @staticmethod
-    def cambio_to_ases(pinta_actual, cantidad_actual: int, pinta_nueva: int) -> tuple[int, bool]:
-        if pinta_nueva != 1:
-            return cantidad_actual, False
-
-        if cantidad_actual % 2 == 0:
-            cantidad_nueva = (cantidad_actual // 2) + 1
-        else:
-            cantidad_nueva = math.ceil(cantidad_actual / 2)
-        return cantidad_nueva, True
+    def cambio_a_ases(cantidad_actual: int) -> int:
+        return (cantidad_actual // 2) + 1 if cantidad_actual % 2 == 0 else math.ceil(cantidad_actual / 2)
 
     @staticmethod
-    def cambio_de_ashes(pinta_actual: int, cantidad_actual: int, pinta_nueva) -> tuple[int, bool]:
-        if pinta_actual != 1:
-            return cantidad_actual, False
-
-        cantidad_nueva = (cantidad_actual * 2) + 1
-        return cantidad_nueva, True
+    def cambio_desde_ases(cantidad_actual: int) -> int:
+        return (cantidad_actual * 2) + 1
 
     @staticmethod
-    def validar_apuesta(pinta: int, cantidad: int) -> bool:
-        # Reglas de rango
-        if not ValidadorApuesta.verificar_rango_pinta(pinta) or not ValidadorApuesta.verificar_rango_cantidad(cantidad):
+    def validar_apuesta_general(pinta: int, cantidad: int) -> bool:
+        return ValidadorApuesta.verificar_rango_pinta(pinta) and ValidadorApuesta.verificar_rango_cantidad(cantidad)
+
+    @staticmethod
+    def validar_primera_apuesta(pinta: int, cantidad: int, jugador_tiene_un_dado: bool) -> bool:
+        if pinta == 1 and not jugador_tiene_un_dado:
             return False
-
-        # No se puede partir con más de un As
-        return pinta != 1 or cantidad <= 1
+        return ValidadorApuesta.validar_apuesta_general(pinta, cantidad)
 
     @staticmethod
     def validar_aumento_apuesta(pinta_actual: int, cantidad_actual: int, pinta_nueva: int, cantidad_nueva: int) -> bool:
-        # Si es la misma pinta
-        if pinta_actual == pinta_nueva:
-            # Si es igual o mayor -> válido
-            # Si la cantidad nueva es menor -> inválido
-            return cantidad_nueva >= cantidad_actual
-        else:
-            # Si la pinta es mayor -> válido
-            # Si la pinta es menor -> inválido
-            return pinta_nueva > pinta_actual
+        # cambios especiales
+        if pinta_nueva == 1 and pinta_actual != 1:
+            return cantidad_nueva >= ValidadorApuesta.cambio_a_ases(cantidad_actual)
+
+        if pinta_actual == 1 and pinta_nueva != 1:
+            return cantidad_nueva >= ValidadorApuesta.cambio_desde_ases(cantidad_actual)
+
+        return cantidad_nueva > cantidad_actual or (cantidad_nueva == cantidad_actual and pinta_nueva > pinta_actual)
