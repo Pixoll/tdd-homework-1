e1f939f0e6789a35bd7be46541052a7c6a4207a8
Pixoll <pixoll.gamer@gmail.com>
2025-09-01 23:43:37 -0400

    RED: test para pinta del dado - falla como esperado

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -7,3 +7,18 @@ def test_tirar_dado() -> None:
     assert 1 <= valor <= 6
     assert 1 <= dado.valor <= 6
     assert valor == dado.valor
+
+
+def test_tirar_dado_pinta() -> None:
+    dado = Dado(1)
+    assert dado.pinta == "As"
+    dado = Dado(2)
+    assert dado.pinta == "Tonto"
+    dado = Dado(3)
+    assert dado.pinta == "Tren"
+    dado = Dado(4)
+    assert dado.pinta == "Cuadra"
+    dado = Dado(5)
+    assert dado.pinta == "Quina"
+    dado = Dado(6)
+    assert dado.pinta == "Sexto"
