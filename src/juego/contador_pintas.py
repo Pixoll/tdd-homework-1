17b76c5565936f99797dd2224eaab24dea4117c2
Pixoll <pixoll.gamer@gmail.com>
2025-09-02 11:39:37 -0400

    GREEN: implementación mínima para contador de pintas con multiples cachos

--- src/juego/contador_pintas.py
+++ src/juego/contador_pintas.py
@@ -4,26 +4,19 @@ from .cacho import Cacho
 
 
 class ContadorPintas:
-    def __init__(self, cacho: Cacho):
-        self._cacho = cacho
+    def __init__(self, cachos: list[Cacho]):
+        self._cachos = cachos
 
     @property
     def valores_dados(self) -> Sequence[int]:
-        return self._cacho.valores_dados
+        valores: list[int] = []
+        for c in self._cachos:
+            for v in c.valores_dados:
+                valores.append(v)
+        return valores
 
     def contar_pinta(self, pinta: int, usar_ases: bool = True) -> int:
-        cantidad = sum(1 for valor in self._cacho.valores_dados if valor == pinta)
         if usar_ases and pinta != 1:
-            ases = sum(1 for valor in self._cacho.valores_dados if valor == 1)
-            if cantidad == 0:
-                # Pinta ausente: máximo 2 ases como comodín
-                cantidad += min(ases, 2)
-            else:
-                # Pinta presente:
-                # regla: solo un as se usa si la pinta tiene un único dado
-                # y todos los ases si la pinta aparece más de una vez
-                if cantidad == 1:
-                    cantidad += 1  # solo un as
-                else:
-                    cantidad += ases  # todos los ases
-        return cantidad
+            return sum(1 for v in self.valores_dados if v == pinta or v == 1)
+        else:
+            return sum(1 for v in self.valores_dados if v == pinta)
