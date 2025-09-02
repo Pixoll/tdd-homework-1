bf5d5eb5c376523972ee9a0a5f795295c28a2d5e
Bandido209 <estebananm2005@gmail.com>
2025-09-01 21:02:18 -0400

    GREEN: implementación mínima para reglas basicas

--- src/juego/validador_apuesta.py
+++ src/juego/validador_apuesta.py
@@ -35,3 +35,32 @@ class ValidadorApuesta:
         cantidad_nueva = (cantidad_actual*2)+1
         
         return (cantidad_nueva, True)
+    
+    @staticmethod
+    def ValidarApuesta(pinta, cantidad):
+        # Reglas de rango
+        if not ValidadorApuesta.VerificarRangoPinta(pinta):
+            return False
+        if not ValidadorApuesta.VerificarRangoCantidad(cantidad):
+            return False
+        
+        # No se puede partir con más de un As
+        if pinta == 1 and cantidad > 1:
+            return False
+        return True
+
+    @staticmethod
+    def ValidarAumentoApuesta(pinta_actual, cantidad_actual, pinta_nueva, cantidad_nueva):
+        # Si es la misma pinta
+        if pinta_actual == pinta_nueva:
+            # Si la cantidad nueva es menor -> inválido
+            if cantidad_nueva < cantidad_actual:
+                return False
+            # Si es igual o mayor -> válido
+            return True
+        else:
+            # Si la pinta es mayor -> válido
+            if pinta_nueva > pinta_actual:
+                return True
+            # Si la pinta es menor -> inválido
+            return False
