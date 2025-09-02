99760441df01a22344d968e33fc1a014f7aa338f
Pixoll <pixoll.gamer@gmail.com>
2025-09-02 13:21:02 -0400

    RED: test para correcto dudado y calzado de arbitro de ronda - falla como esperado

--- tests/test_arbitro_ronda.py
+++ tests/test_arbitro_ronda.py
@@ -1,7 +1,24 @@
-from src.juego.arbitro_ronda import ArbitroRonda, ResultadoDuda
+from src.juego.arbitro_ronda import ArbitroRonda
 from src.juego.cacho import Cacho
 from src.juego.contador_pintas import ContadorPintas
 from src.juego.dado import Dado
+
+
+def test_cantidad_total_dados():
+    cacho1 = Cacho([Dado(2), Dado(2), Dado(3), Dado(4), Dado(5)])
+    cacho2 = Cacho([Dado(3), Dado(3), Dado(4), Dado(5), Dado(6)])
+    contador = ContadorPintas([cacho1, cacho2])
+    arbitro = ArbitroRonda(contador)
+    assert arbitro.total_dados_en_juego == 10
+
+
+def test_cantidad_total_dados_tras_remover():
+    cacho1 = Cacho([Dado(2), Dado(2), Dado(3), Dado(4), Dado(5)])
+    cacho2 = Cacho([Dado(3), Dado(3), Dado(4), Dado(5), Dado(6)])
+    cacho1.remover_dado()
+    contador = ContadorPintas([cacho1, cacho2])
+    arbitro = ArbitroRonda(contador)
+    assert arbitro.total_dados_en_juego == 9
 
 
 # Test: duda correcta (apuesta menor que cantidad real)
@@ -13,21 +30,8 @@ def test_duda_correcta_gana_jugador():
 
     cantidad_apostada = 1
     pinta = 2
-    resultado = arbitro.dudar(cantidad_apostada, pinta)
-    assert resultado == ResultadoDuda.PIERDE
-
-
-# Test: duda exacta (calzar)
-def test_duda_correcta_calzar():
-    cacho1 = Cacho([Dado(2), Dado(2), Dado(1), Dado(3), Dado(4)])
-    cacho2 = Cacho([Dado(3), Dado(3), Dado(2), Dado(4), Dado(5)])
-    contador = ContadorPintas([cacho1, cacho2])
-    arbitro = ArbitroRonda(contador)
-
-    cantidad_apostada = 4
-    pinta = 3
-    resultado = arbitro.dudar(cantidad_apostada, pinta)
-    assert resultado == ResultadoDuda.CALZAR
+    resultado = arbitro.dudar(cantidad_apostada, pinta, usar_ases=True)
+    assert resultado == False
 
 
 # Test: duda con ases como comodín
@@ -39,8 +43,8 @@ def test_duda_con_ases_como_comodin():
 
     cantidad_apostada = 2
     pinta = 3
-    resultado = arbitro.dudar(cantidad_apostada, pinta)
-    assert resultado == ResultadoDuda.PIERDE
+    resultado = arbitro.dudar(cantidad_apostada, pinta, usar_ases=True)
+    assert resultado == False
 
 
 # Test: duda sin ases, apuesta exacta
@@ -52,8 +56,8 @@ def test_duda_sin_ases_apuesta_exacta():
 
     cantidad_apostada = 2
     pinta = 2
-    resultado = arbitro.dudar(cantidad_apostada, pinta)
-    assert resultado == ResultadoDuda.CALZAR
+    resultado = arbitro.dudar(cantidad_apostada, pinta, usar_ases=False)
+    assert resultado == False
 
 
 # Test: duda con apuesta menor y ases presentes
@@ -65,8 +69,8 @@ def test_duda_con_ases_apuesta_menor():
 
     cantidad_apostada = 1
     pinta = 2
-    resultado = arbitro.dudar(cantidad_apostada, pinta)
-    assert resultado == ResultadoDuda.PIERDE
+    resultado = arbitro.dudar(cantidad_apostada, pinta, usar_ases=True)
+    assert resultado == False
 
 
 def test_duda_con_ases_apuesta_mayor():
@@ -77,34 +81,77 @@ def test_duda_con_ases_apuesta_mayor():
 
     cantidad_apostada = 3
     pinta = 2
-    resultado = arbitro.dudar(cantidad_apostada, pinta)
-    assert resultado == ResultadoDuda.GANA
+    resultado = arbitro.dudar(cantidad_apostada, pinta, usar_ases=True)
+    assert resultado == True
 
 
 def test_si_puede_calzar():
-    cacho1 = Cacho([Dado(1), Dado(2), Dado(3), Dado(4), Dado(5)])
-    cacho2 = Cacho([Dado(2), Dado(3), Dado(4), Dado(5), Dado(6)])
+    cacho1 = Cacho([Dado(1), Dado(2), Dado(2), Dado(2), Dado(2)])
+    cacho2 = Cacho([Dado(2), Dado(3), Dado(3), Dado(3), Dado(3)])
     contador = ContadorPintas([cacho1, cacho2])
     arbitro = ArbitroRonda(contador)
 
-    cantidad_apostada = 3
-    puede_calzar = arbitro.puede_calzar(cantidad_apostada)
+    cantidad_apostada = 5
+    dados_jugador = 5
+    puede_calzar = arbitro.puede_calzar(cantidad_apostada, dados_jugador)
     assert puede_calzar == True
 
 
 def test_no_puede_calzar():
-    cacho1 = Cacho([Dado(1), Dado(2), Dado(3), Dado(4), Dado(5)])
-    cacho2 = Cacho([Dado(2), Dado(3), Dado(4), Dado(5), Dado(6)])
-    cacho1.remover_dado()
-    cacho1.remover_dado()
-    cacho1.remover_dado()
-    cacho2.remover_dado()
-    cacho2.remover_dado()
-    cacho2.remover_dado()
-
+    cacho1 = Cacho([Dado(1), Dado(2), Dado(2), Dado(2), Dado(2)])
+    cacho2 = Cacho([Dado(2), Dado(3), Dado(3), Dado(3), Dado(3)])
     contador = ContadorPintas([cacho1, cacho2])
     arbitro = ArbitroRonda(contador)
 
-    cantidad_apostada = 9
-    puede_calzar = arbitro.puede_calzar(cantidad_apostada)
+    cantidad_apostada = 4
+    dados_jugador = 5
+    puede_calzar = arbitro.puede_calzar(cantidad_apostada, dados_jugador)
     assert puede_calzar == False
+
+
+def test_calzar_correcto_sin_ases():
+    cacho1 = Cacho([Dado(1), Dado(2), Dado(2), Dado(2), Dado(2)])
+    cacho2 = Cacho([Dado(2), Dado(3), Dado(3), Dado(3), Dado(3)])
+    contador = ContadorPintas([cacho1, cacho2])
+    arbitro = ArbitroRonda(contador)
+
+    cantidad_apostada = 5
+    pinta = 2
+    resultado = arbitro.calzar(cantidad_apostada, pinta, usar_ases=False)
+    assert resultado == True
+
+
+def test_calzar_correcto_con_ases():
+    cacho1 = Cacho([Dado(1), Dado(2), Dado(2), Dado(2), Dado(2)])
+    cacho2 = Cacho([Dado(2), Dado(3), Dado(3), Dado(3), Dado(3)])
+    contador = ContadorPintas([cacho1, cacho2])
+    arbitro = ArbitroRonda(contador)
+
+    cantidad_apostada = 6
+    pinta = 2
+    resultado = arbitro.calzar(cantidad_apostada, pinta, usar_ases=True)
+    assert resultado == True
+
+
+def test_calzar_incorrecto_sin_ases():
+    cacho1 = Cacho([Dado(1), Dado(2), Dado(2), Dado(2), Dado(2)])
+    cacho2 = Cacho([Dado(2), Dado(3), Dado(3), Dado(3), Dado(3)])
+    contador = ContadorPintas([cacho1, cacho2])
+    arbitro = ArbitroRonda(contador)
+
+    cantidad_apostada = 5
+    pinta = 3
+    resultado = arbitro.calzar(cantidad_apostada, pinta, usar_ases=False)
+    assert resultado == False
+
+
+def test_calzar_incorrecto_con_ases():
+    cacho1 = Cacho([Dado(1), Dado(2), Dado(2), Dado(2), Dado(2)])
+    cacho2 = Cacho([Dado(2), Dado(3), Dado(3), Dado(3), Dado(3)])
+    contador = ContadorPintas([cacho1, cacho2])
+    arbitro = ArbitroRonda(contador)
+
+    cantidad_apostada = 4
+    pinta = 3
+    resultado = arbitro.calzar(cantidad_apostada, pinta, usar_ases=True)
+    assert resultado == False
